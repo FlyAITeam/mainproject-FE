2a57f04e1db2a226b84b0516307c95961a52a9ef
20161609 <dksshwns@naver.com>
2024-08-19 20:14:15 +0900

    refactor: 비밀번호 검증 및 access token 존재 여부 확인 함수 개선 #6

--- front/src/app/feature_test/page.jsx
+++ front/src/app/feature_test/page.jsx
@@ -1,54 +1,53 @@
-// "use client";
-// import React from 'react';
-// import { getUserInfo } from '@/libs/authController';
-
-// const GetUserInfoPage = () => {
-//   const handleGetUserInfo = async () => {
-//     try {
-//       const response = await getUserInfo();
-//       console.log('User info retrieved successfully:', response);
-//     } catch (error) {
-//       console.error('Error retrieving user info:', error);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={handleGetUserInfo}>Get User Info</button>
-//     </div>
-//   );
-// };
-
-// export default GetUserInfoPage;
-// -> 로그인한 사용자의 정보를 가져오는 페이지를 만들어보자.
-// -> getUserInfo 함수를 사용하여 사용자 정보를 가져온다.
-
 "use client";
 import React from 'react';
-import { checkPasswd } from '@/libs/authController';
+import { loginUser, isTokenAvailable } from '@/libs/authController';
+import { registerDog } from '@/libs/dogInfo';
 
-const LoginPage = () => {
-  const handleLogin = async () => {
-    const pwd_list = ["123982a!", "1238", "21378", "hjk23", "239knl", "123!@@3"];
-    for (let i = 0; i < pwd_list.length; i++) {
-      console.log(pwd_list[i], checkPasswd(pwd_list[i]));
+const Page = () => {
+  // 1. 사용자 로그인
+  const loginHandle = async () => {
+    try {
+      const data = await loginUser('decomia12', '1234');
+      console.log('로그인 성공:', data);
+    } catch (error) {
+      console.error('로그인 중 오류:', error);
     }
-  
-    // try {
-    //   const response = await loginUser('pocketguy1', '1234');
-    //   console.log('Login successful:', response);
-    // }
-    // catch (error) {
-    //   console.error('Error logging in:', error);
-    // }
-
   };
 
+  // 2. 토큰 확인
+  const isTokenAvailableHandle = async () => {
+    try {
+      const data = isTokenAvailable();
+      console.log('토큰 확인 성공:', data);
+    } catch (error) {
+      console.error('토큰 확인 중 오류:', error);
+    }
+  };
+
+  // 3. 강아지 정보 등록
+  const handle = async () => {
+    try {
+      // registerDog
+      const data = await registerDog('maru', 'poodle', 1, 23, 'male', 15.0);
+      console.log('강아지 정보 등록 성공:', data);
+    } catch (error) {
+      console.error('강아지 정보 등록 중 오류:', error);
+    }
+  };
+  
   return (
     <div>
-      <button onClick={handleLogin}>Login</button>
+      <div>
+        <button onClick={loginHandle}>login</button>
+      </div>
+      <div>
+        <button onClick={isTokenAvailableHandle}>isTokenAvailable</button>
+      </div>
+      <div>
+        <button onClick={handle}>handle</button>
+      </div>
     </div>
   );
-}
+};
 
-export default LoginPage;
+export default Page;
